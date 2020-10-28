8ee1bf03505ae5679a1eafd63aa9b79150c64b2e
Aliasad9 <aliasad6521@gmail.com>
2020-10-29 01:33:50 +0500

    Teams Minor UI changes

--- lib/widgets/team_member_card.dart
+++ lib/widgets/team_member_card.dart
@@ -34,93 +34,100 @@ class TeamMemberCard extends StatelessWidget {
                 ),
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  child: Text(
-                    '$name',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: 'OpenSans',
-                    ),
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '$role at',
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'OpenSans',
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 4),
-                      decoration: BoxDecoration(
-                          color:
-                              Theme.of(context).primaryColor.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(50)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text(
-                          '$company',
-                          style: TextStyle(
-                            fontSize: 10,
-                            fontWeight: FontWeight.w700,
-                            fontFamily: 'OpenSans',
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            Container(
+              width: MediaQuery.of(context).size.width - 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 16.0,
-                      top: 6,
-                    ),
-                    child: Text(
-                      'LVL \u2022 $level',
-                      style: TextStyle(
-                        color: Colors.black38,
-                        fontSize: 10,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'OpenSans',
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-
-                    },
-                    child: Container(
-                      child: Flexible(
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
                         child: Text(
-                          "View Profile",
+                          '$name',
                           style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 10,
-                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
                             fontFamily: 'OpenSans',
                           ),
                         ),
                       ),
-                    ),
+                      Row(
+                        children: [
+                          Text(
+                            '$role at',
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'OpenSans',
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 4),
+                            decoration: BoxDecoration(
+                                color: Theme.of(context)
+                                    .primaryColor
+                                    .withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Text(
+                                '$company',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: 'OpenSans',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 16.0,
+                            top: 6,
+                          ),
+                          child: Text(
+                            'LVL \u2022 $level',
+                            style: TextStyle(
+                              color: Colors.black38,
+                              fontSize: 10,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'OpenSans',
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            child: Flexible(
+                              child: Text(
+                                "View Profile",
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: 'OpenSans',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
