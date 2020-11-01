37e5b199afaa578330ccd2a8bd25b785823c0872
python1951 <qamarha28@gmail.com>
2020-11-01 23:34:08 +0500

    create_new_team_ui

--- lib/screens/create_new_team.dart
+++ lib/screens/create_new_team.dart
@@ -12,7 +12,7 @@ class _CreateNewTeamState extends State<CreateNewTeam> {
   TextEditingController _nameController = new TextEditingController();
   TextEditingController _descriptionController = new TextEditingController();
 
-  final databaseReference = FirebaseFirestore.instance;
+ final databaseReference = FirebaseFirestore.instance;
 
   Map<int, TextEditingController> controllers = {};
 
@@ -24,9 +24,21 @@ class _CreateNewTeamState extends State<CreateNewTeam> {
       _fieldsCount += 1;
       var controller =
           controllers.putIfAbsent(_fieldsCount, () => TextEditingController());
-      listOfFields.add(TextFormField(
-        controller: controller,
-        decoration: InputDecoration(hintText: 'role name'),
+      listOfFields.add(Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          validator: (value) {
+            if (value.isEmpty) {
+              return ("Please provide a valid role");
+            }
+            return null;
+          },
+
+          controller: controller,
+          decoration: InputDecoration(labelText: 'role name',   filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(9)),),
+        ),
       ));
     });
   }
@@ -35,17 +47,73 @@ class _CreateNewTeamState extends State<CreateNewTeam> {
   void initState() {
     super.initState();
     listOfFields = <Widget>[
-      TextFormField(
-        controller: _nameController,
-        decoration: InputDecoration(
-            hintText: 'title'), //TODO: put empty string validator
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          validator: (value) {
+            if (value.isEmpty) {
+              return ("Please provide a valid team name");
+            }
+            return null;
+          },
+
+          controller: _nameController,
+          decoration: InputDecoration(
+
+    filled: true,
+    fillColor: Colors.white,
+    border: OutlineInputBorder(borderRadius: BorderRadius.circular(9)),
+
+
+              labelText: 'title'), //TODO: put empty string validator
+        ),
       ),
-      TextFormField(
-        controller: _descriptionController,
-        decoration: InputDecoration(hintText: 'description'),
+      SizedBox(height:5),
+      Padding(
+                padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          validator: (value) {
+            if (value.isEmpty) {
+              return ("Please provide a valid description");
+            }
+            return null;
+          },
+          controller: _descriptionController,
+          decoration: InputDecoration(labelText: 'description',
+
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(9)),
+
+          ),
+        ),
       )
     ];
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -83,27 +151,70 @@ class _CreateNewTeamState extends State<CreateNewTeam> {
               ),
               Row(
                 children: [
-                  IconButton(
-                      color: Colors.red,
-                      icon: Icon(Icons.remove),
-                      onPressed: () {
-                        setState(() {
-                          if (listOfFields.length > 2) {
-                            listOfFields.removeAt(listOfFields.length - 1);
-                            _fieldsCount -= 1;
-                          }
-                        });
-                      }),
-                  IconButton(
-                    color: Colors.green,
-                    onPressed: () {
-                      addNewField();
-                    },
-                    icon: Icon(Icons.add),
-                  )
+                  Padding(
+                    padding: const EdgeInsets.only(top:8.0,left:140,right: 4),
+                    child: ClipOval(
+                      child: Material(
+                        color:  Colors.blue,
+                        child: InkWell(
+                          splashColor: Colors.red,
+                          child: SizedBox(
+                            child: Icon(Icons.remove),
+                            width: 56,
+                            height: 56,
+                          ),
+                            onTap: () {
+                              setState(() {
+                                if (listOfFields.length > 2) {
+                                  listOfFields.removeAt(listOfFields.length - 1);
+                                  _fieldsCount -= 1;
+                                }
+                              });
+                            }
+                        ),
+                      ),
+
+
+
+                       ),
+                  ),
+                  SizedBox(
+                    width:5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:8.0),
+                    child: ClipOval(
+                      child: Material(
+                        color:  Colors.blue,
+                        child: InkWell(
+                            splashColor: Colors.green,
+                            child: SizedBox(
+                              child: Icon(Icons.add),
+                              width: 56,
+                              height: 56,
+                            ),
+                            onTap: () {
+                             addNewField();
+                            }
+                        ),
+                      ),
+
+
+
+                    ),
+                  ),
+
                 ],
               ),
-              FlatButton(
+              SizedBox(height: 5,),
+              Center(
+                child: RaisedButton(
+                  color: Colors.redAccent,
+                    padding: const EdgeInsets.all(10),
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16.0))),
+
                   onPressed: () async {
                     var companyName = _nameController.text;
                     await databaseReference.collection('Companies').add({
@@ -122,7 +233,8 @@ class _CreateNewTeamState extends State<CreateNewTeam> {
                     print('Done');
                     Navigator.pop(context);
                   },
-                  child: Text('Submit'))
+                    child: Text('Submit')),
+              )
             ],
           ),
         ),
